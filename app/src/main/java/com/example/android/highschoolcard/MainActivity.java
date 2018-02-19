e9ce21186138ee99aed55a8b6e09febea0199cc6
minut1bc <minut1@yahoo.com>
2018-02-19 22:06:54 +0200

    Initial commit

---
+++ app/src/main/java/com/example/android/highschoolcard/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.android.highschoolcard;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void openBrowser(View view){
+        String url = (String)view.getTag();
+
+        Intent intent = new Intent();
+        intent.setAction(Intent.ACTION_VIEW);
+        intent.addCategory(Intent.CATEGORY_BROWSABLE);
+
+        intent.setData(Uri.parse(url));
+
+        startActivity(intent);
+
+    }
+
+}
